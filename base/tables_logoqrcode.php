6291059e1eca807b22c93eef194823796b21a819
Remi Suinot <remi@suinot.org>
2019-03-29 09:30:19 +0100

    renommé :         tables_logoqrcode.php -> base/tables_logoqrcode.php 	modifié :         logoqrcode_administrations.php
    La base de données doit à présent, fonctionner correctement
    (normalement!?)  => création/modification/supression

--- base/tables_logoqrcode.php
+++ base/tables_logoqrcode.php
@@ -4,22 +4,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function declarer_tables_logoqrcodes_sql($tables){
 	$tables['spip_logoqrcode'] = array(
-	
 		'principale' => "oui",
 		'field'=> array(
 			"id_logoqrcode"	=> "bigint(21) NOT NULL",
-			"taille"	=> "int  NOT NULL",
-			"ecc"	=> "tinytext DEFAULT '' NOT NULL",
-			"rubrique" => "bigint(21) NOT NULL DEFAULT 0",
-			"article" => "bigint(21) NOT NULL DEFAULT 0",
-			"maj"	=> "TIMESTAMP"
+			"taille"	=> "int(2)  NOT NULL",
+			"ecc"		=> "tinytext DEFAULT '' NOT NULL",
+			"rubrique"	=> "int(1) NOT NULL DEFAULT 0",
+			"article"	=> "int(1) NOT NULL DEFAULT 0",
+			"maj"		=> "TIMESTAMP"
 		),
 	);
 	
 	return $tables;
 }
-
-
 
 function declarer_tables_interfaces($interfaces) {
 	$interfaces['table_des_tables']['logoqrcode'] = 'logoqrcode';
--- logoqrcode_administrations.php
+++ logoqrcode_administrations.php
@@ -31,17 +31,28 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function logoqrcode_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
-	include_spip('base/upgrade');
-
 	# Premiere installation  creation des tables
-	$maj['create'] = array
-		array(<ici, mes champs de base de données>
-		array('<autre champs?>'),
+	$maj['create'] = array(
+		array('maj_tables', array('spip_logoqrcode')),
 		);
 
+	// Mise à jour si besoin dans le futur: (voir plugin GIS pour plus de détail si besoin)
+	// $maj['1.0.5'] = array(
+	// 	// ajout de la nouvelle table
+	// 	array('maj_tables', array('spip_logoqrcode')),
+	// 	// on renomme <chanp_a_modifier> en <nouveau>
+	// 	array('sql_alter', 'TABLE spip_logoqrcode CHANGE <champ_à_modifier> <nouveau> <type(long/float/...>(x) NULL NULL'),
+	//	);
+
+	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
+function logoqrcode_upgrade_1_0_5() {
+	// ici, toutes les modification de la base de données nécessaire en cas d'upgrade 
+	// le numéro de la fonction ce retrouve dans le numero d'upgrade!
+
+}
 
 /**
  * Fonction de désinstallation du plugin Logo-qrcode.
@@ -55,5 +66,8 @@ function logoqrcode_upgrade($nom_meta_base_version, $version_cible) {
 **/
 function logoqrcode_vider_tables($nom_meta_base_version) {
 
+	sql_drop_table('spip_logoqrcode');
 	effacer_meta($nom_meta_base_version);
+	// effacer configuration?
+	effacer_meta('logoqrcode');
 }
