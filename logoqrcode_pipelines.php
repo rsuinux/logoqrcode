bf01bab9b728eb3a6f71e786181bf0219705fac9
Remi Suinot <remi@suinot.org>
2019-05-09 19:26:42 +0200

    nouveau fichier : logoqrcode_pipelines.php 	modifié :         paquet.xml 	supprimé :        logoqrcode_pipeline.php

---
+++ logoqrcode_pipelines.php
@@ -0,0 +1,42 @@
+<?php                                                                                                                                                                                            
+/**                                                                                                                                                                                              
+ * Utilisations de pipelines par logoqrcode
+ *
+ * @plugin     Logo-qrcode
+ *
+ * @copyright  2018-1019
+ *
+ * @author     Rémi Suinot
+ * @licence    GNU/GPL
+ *
+ * @package    SPIP\logoqrcode\Pipelines
+ *
+ */                                                                                                                                                                                              
+
+if (!defined('_ECRIRE_INC_VERSION')) return;                                                                                                                                                     
+
+/**
+ * création du qrcode en fonction des parametres de l'administrateur.
+ * le nom de l'image dépend du numero de l'article/rubrique
+ * => composition du nom "article_<id article>.png
+ * => composition du nom "rubrique_<id rubrique>.png
+ *
+ * @param adresse article/rubrique id_article/id_rubrique
+ * @return adresse image
+ */
+
+function creation_qrcode(){
+	// On commence par récupérer l'id article ou rubrique :
+	$id_article = $GLOBALS['id_article'];
+	$id_rubrique = $GLOBALS['id_rubrique'];
+	return;
+}
+
+/*
+ * on surcharde logo_article
+ * (ou on essai)
+ * $id adresse de l'article
+ * @return adresse de l'image
+ */
+
+?>
