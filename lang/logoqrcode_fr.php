86bf83c1a09b70caa1af28a2fcc3920c588f5426
remi <remi@suinot.org>
2019-05-27 18:26:24 +0200

    modifié :         formulaires/configurer_logoqrcode.html 	nouveau fichier : formulaires/configurer_logoqrcode.html.orig 	modifié :         lang/logoqrcode_fr.php 	modifié :         logoqrcode_options.php

--- lang/logoqrcode_fr.php
+++ lang/logoqrcode_fr.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')){
@@ -27,6 +27,7 @@ permettent de récuperer des informations, dont des URL, sans avoir à les saisi
 	'explication_articles' => 'Faut il calculer et afficher les QrCode pour chaque rubrique?',
 	'explication_ecc_aide' => 'Ce pourcentage indique combien d\'erreurs peuvent être récupérées en cas de difficulté de lecture du QRcode.',
 	'explication_taille_aide' => 'Taille de chacun des carré constituant le QrCode.',
+	'explication_reprise' => 'le plugin va placer un qrcode sur tous les articles/rubrique (en fonction du parametrage) déjà présent. <color=\"red\">ATTENTION:</color> cela supprime les anciens logo',
 	'explication_rubriques' => 'Faut il calculer et afficher les QrCode pour chaque article?',
 	'ecc_restauration_l' => 'L (7% de restauration)',
 	'ecc_restauration_m' => 'M (15% de restauration)',
@@ -39,6 +40,7 @@ permettent de récuperer des informations, dont des URL, sans avoir à les saisi
 	// L
 	'label_articles' => 'QrCode sur article',
 	'label_ecc' => 'Correction d\'erreurs',
+	'label_reprise' => 'reprendre les anciens articles et y mettre un qrcode',
 	'label_rubriques' => 'QrCode sur rubrique',
 	'label_taille' => 'Taille',
 
--- logoqrcode_options.php
+++ logoqrcode_options.php
@@ -4,17 +4,6 @@
 if (!defined('_ECRIRE_INC_VERSION'))
         return;
 
-// Pour forcer les logs du plugin, outil actif ou non :
-define('_LOG_CS_FORCE', 'oui');
-if (defined('_LOG_CS')) {
-        cs_log(str_repeat('-', 80), '', sprintf('LOGOQRCODE . [#%04X]. ', rand()));
-        cs_log('INIT : logoqrcode_options, ' . $_SERVER['REQUEST_URI']);
-}
-
-// Logs de tmp/spip.log -> deja créé dans le plugin bonux
-// function cs_log($variable, $prefixe = '', $stat = '') {
-
-cs_log("  -- test de log ?");
 
 ?>
 
